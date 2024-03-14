21a4d0075b2a57801f269320849bb3df88c239e1
sametdemiralay <demiralayahmetsamet@gmail.com>
2024-03-14 20:21:55 +0300

    updated about page and navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+const API_KEY = process.env.API_KEY;
+
 const Home = () => {
-  return <div>HELLO WRLD</div>;
+  return (
+    <div>
+      <h1>JUICE WRLD</h1>
+      <h1>{API_KEY}</h1>
+    </div>
+  );
 };
 
 export default Home;
